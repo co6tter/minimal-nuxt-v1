240cfb9bcbf4a46776d3017581f6eabcca2dd255
co6tter <co6tter@gmail.com>
2025-04-28 07:53:44 +0900

    feat: add products

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,50 @@
 <script setup lang="ts">
+import type { Product } from "~/types";
+
+const products = useState<Product[]>("products");
+
 useHead({
-  title: "Index page",
+  title: "Products",
 });
 </script>
 
 <template>
-  <p>Index page</p>
-  <NuxtLink to="/about">About</NuxtLink>
+  <section class="products">
+    <h2>Products</h2>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <NuxtLink :to="`/products/${product.id}`">
+          <img src="https://placehold.jp/300x300.png" :alt="product.name" />
+          <h3>{{ product.name }}</h3>
+          <p>{{ product.price.toLocaleString() }}円</p>
+        </NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<style scoped>
+.products {
+  h2 {
+    margin-top: 2rem;
+  }
+  ul {
+    margin: 2rem 0 0;
+    padding: 0;
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 2rem;
+  }
+  a {
+    display: block;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+  h3 {
+    margin-top: 0.5rem;
+  }
+}
+</style>
